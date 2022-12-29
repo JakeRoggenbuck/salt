7b19aa152f85c0162316d45d4d60799fb5b710dd
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-12-28 16:25:09 -0800

    Use the two

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    printf("argc: %d\n", argc);
+
+    if (argc >= 1) {
+        printf("argv[0]: %s\n", argv[0]);
+    }
+
     printf("Salt\n");
 
     return 0;
