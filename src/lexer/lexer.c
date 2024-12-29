6b1b98ee196b7cb0ecda3e1f8751ae4bd529444a
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2024-12-29 01:55:36 -0800

    Check malloc

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -7,8 +7,8 @@
  * Adam Hutchings' https://github.com/y-flat/yfc
  */
 #include "lexer.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int salt_getc(struct salt_lexer *lexer);
 int salt_ungetc(struct salt_lexer *lexer, int c);
@@ -18,5 +18,11 @@ int salt_skip_comment(struct salt_lexer *lexer);
 
 void salt_init(struct salt_lexer *lexer, char *input) {
     lexer->token_arr = malloc(10 * sizeof(struct salt_token));
+
+    if (lexer->token_arr == NULL) {
+        fprintf(stderr, "Malloc failed.\n");
+        exit(1);
+    }
+
     printf("input: %s", input);
 }
